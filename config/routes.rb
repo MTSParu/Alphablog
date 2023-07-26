d93c9be607c027074da727b2bac57d0e27c61014
Talha <johndoe@example.com>
2023-07-26 13:37:27 +0500

    Add about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
+  get 'contact_us', to: 'contact#contact_us'
+  get 'services', to: 'services#services'
+  get 'hire', to: 'index#hire'
 end
